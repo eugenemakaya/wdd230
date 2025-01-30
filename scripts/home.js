f807b1b1b34b05f87cb7d2d205f77cccd679ea6f
eugenemakaya <eugene.mcayer@gmail.com>
2025-01-30 19:47:19 +0200

    week 2 home enhancements: final tweak

--- scripts/home.js
+++ scripts/home.js
@@ -8,6 +8,7 @@ document.getElementById("year").innerHTML = new Date().getFullYear();
 
 // Find the footer element by its ID
 const footerYearElement = document.getElementById('lastModified');
+console.log(footerYearElement);
 
 // Set the text content of the footer element to the current year
 if (footerYearElement) {
