a6c48b35a7057a9c24c5e7f50e29c9852f58046f
eugenemakaya <eugene.mcayer@gmail.com>
2025-01-30 17:19:58 +0200

    home enhancements

---
+++ scripts/home.js
@@ -0,0 +1,21 @@
+const ham = document.querySelector("#menu");
+const nav = document.querySelector("nav");
+
+// Dynamiclly inject current year and date last modified in html
+
+// update year span element
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
+// Find the footer element by its ID
+const footerYearElement = document.getElementById('lastModified');
+
+// Set the text content of the footer element to the current year
+if (footerYearElement) {
+    footerYearElement.innerHTML = `Last Updated: ${document.lastModified}`;
+}
+
+// responsive ham and nav
+ham.addEventListener("click", () => {
+  ham.classList.toggle("show");
+  nav.classList.toggle("show");
+});
