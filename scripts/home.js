eca4c2c68d7c9bd97a00391a076584fc931e0455
eugenemakaya <eugene.mcayer@gmail.com>
2025-02-01 13:56:05 +0200

    responsive images and map

--- scripts/home.js
+++ scripts/home.js
@@ -20,3 +20,22 @@ ham.addEventListener("click", () => {
   ham.classList.toggle("show");
   nav.classList.toggle("show");
 });
+
+// initialize display element variable
+const visitsDisplay = document.querySelector("#visits");
+
+// Get the stored VALUE for the numVisits-ls KEY in localStorage if it exists. If the numVisits KEY is missing, then assign 0 to the numVisits variable.
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+// Determine if this is the first visit or display the number of visits. We wrote this example backwards in order for you to think deeply about the logic.
+if (numVisits !== 0) {
+	visitsDisplay.innerHTML = `Visits Count: ${numVisits}`;
+} else {
+	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+
+// increment the number of visits by one.
+numVisits++;
+
+// store the new visit total into localStorage, key=numVisits-ls
+localStorage.setItem("numVisits-ls", numVisits);
