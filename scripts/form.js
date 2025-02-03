9eea0e17b781bf73bbcebc486160d57e30628291
eugenemakaya <eugene.mcayer@gmail.com>
2025-02-03 11:01:31 +0200

    Form: dev standards tweaks

--- scripts/form.js
+++ scripts/form.js
@@ -1,6 +1,28 @@
 const p1 = document.getElementById("password");
 const p2 = document.getElementById("repeat_password");
 const fb = document.getElementById("feedback");
+const ham = document.querySelector("#menu");
+const nav = document.querySelector("nav");
+
+// Dynamiclly inject current year and date last modified in html
+
+// update year span element
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
+// Find the footer element by its ID
+const footerYearElement = document.getElementById('lastModified');
+console.log(footerYearElement);
+
+// Set the text content of the footer element to the current year
+if (footerYearElement) {
+    footerYearElement.innerHTML = `Last Updated: ${document.lastModified}`;
+}
+
+// responsive ham and nav
+ham.addEventListener("click", () => {
+  ham.classList.toggle("show");
+  nav.classList.toggle("show");
+});
 
 // Password match validation
 p2.addEventListener("focusout", controller);
